4ba8ed99b07635ad5df5a6d588633c9ea213d05a
Borys-Grinberg <borys.grinberg@gmail.com>
2023-10-19 19:11:26 +0200

    UPDATE-ALL

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,34 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, "0")}`;
 }
+
+const controls = document.getElementById("controls");
+const input = controls.querySelector("input");
+const createButton = controls.querySelector("[data-create]");
+const destroyButton = controls.querySelector("[data-destroy]");
+
+const boxes = document.getElementById("boxes");
+
+function createBoxes(amount) {
+  const initialSize = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${initialSize + i * 10}px`;
+    box.style.height = `${initialSize + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+createButton.addEventListener("click", () => {
+  const amount = input.value;
+  createBoxes(amount);
+});
+
+destroyButton.addEventListener("click", destroyBoxes);
